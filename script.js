b640bbb6e166e634f81c3251e682424b7d153534
MASUR89 <surmava9@gmail.com>
2025-10-03 23:19:50 +0400

    styling

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 //rejex 
-const pattern = /^A/
-const testString = "Ahoj"
-console.log(pattern.test(testString)) // true
+// const pattern = /^A/
+// const testString = "Ahoj"
+// console.log(pattern.test(testString)) // true
 
 
 
-const phonePattern = /^\+41 \d{3} \d{3} \d{2} \d{2}$/
-const phoneNumber = "+41 999 888 77 66"       
-console.log(phonePattern.test(phoneNumber)) 
+// const phonePattern = /^\+41 \d{3} \d{3} \d{2} \d{2}$/
+// const phoneNumber = "+41 999 888 77 66"       
+// console.log(phonePattern.test(phoneNumber)) 
 
 
 
 
-const namePattern = /^[A-Za-zÀ-ÖØ-öø-ÿ' -]{2,50}$/;
-const testName = "Mama Mia";
-console.log(namePattern.test(testName)); 
+// const namePattern = /^[A-Za-zÀ-ÖØ-öø-ÿ' -]{2,50}$/;
+// const testName = "Mama Mia";
+// console.log(namePattern.test(testName)); 
 
 
-const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-const email = "user@gmail.com";
-console.log(emailPattern.test(email));
+// const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+// const email = "user@gmail.com";
+// console.log(emailPattern.test(email));
 
 
-const strongPasswordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{12,}$/;
-const strongPassword = "StrongP@ssw0rd";
-console.log(strongPasswordPattern.test(strongPassword)); 
+// const strongPasswordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{12,}$/;
+// const strongPassword = "StrongP@ssw0rd";
+// console.log(strongPasswordPattern.test(strongPassword)); 
 
 
-const easyPasswordPattern = /^[A-Za-z0-9]{6,}$/;
-const easyPassword = "abc123";
-console.log(easyPasswordPattern.test(easyPassword)); 
+// const easyPasswordPattern = /^[A-Za-z0-9]{6,}$/;
+// const easyPassword = "abc123";
+// console.log(easyPasswordPattern.test(easyPassword)); 
